a7a4638745a2c23de3bcf46f0c3ee02c6182c5e3
Eric-Le-Ge <ericlege@berkeley.edu>
2018-12-12 21:30:59 -0800

    Register

--- src/component/App.js
+++ src/component/App.js
@@ -1,2 +1,18 @@
-import * as math from "../lib/math";
-alert("2π = " + math.sum(math.pi, math.pi));
+
+import React, { Component } from 'react';
+import { TopBar } from './TopBar';
+import { Main } from './Main';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <TopBar />
+                <Main />
+            </div>
+        );
+    }
+}
+
+export default App;
+
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './component/App';
+import { TopBar } from "./component/TopBar";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
